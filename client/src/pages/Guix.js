1d8b73d2d9a252de63a464478758173efe4dbca4
Jack <jackgr83@gmail.com>
2020-08-17 22:50:49 -0400

    order modal

--- client/src/pages/Guix.js
+++ client/src/pages/Guix.js
@@ -1,66 +1,73 @@
 import React, { useState, useEffect } from 'react';
 import { navigate } from 'hookrouter'
 import Ecuador from '../img/Ecuador.png'
+import TextPopup from '../components/ui/TextPopup/TextPopup'
 import Cart from './Cart'
 
 const Guix = () => {
 
-    const [hovered1, setHovered1] = useState(false)
-    const [hovered2, setHovered2] = useState(false)
-    const [hovered3, setHovered3] = useState(false)
-    const [hovered4, setHovered4] = useState(false)
-    const [isClicked, setIsClicked] = useState(false)
+    const [hovered, setHovered] = useState(false)
+    const [textPopup, chTextPopup] = useState(false)
 
-    // const onClick = () => {
-    //     setIsClicked(true)
-    // }
+    const showTextPopup = () => {
+        chTextPopup(true)
+    }
+    const hideTextPopup = () => {
+        chTextPopup(false)
+    }
 
     const cart = <Cart />
-    const baseStyle = {color: "red", position: "absolute", left: "25%", top: "50%"}
-    const hoverStyle = {fontSize: "30px", color: "red", position: "absolute", left: "25%", top: "50%"}
+    const baseStyle = {opacity: "80%", position: "absolute", left: "37%", top: "50%"}
+    const hoverStyle = {fontSize: "30px", opacity: "80%", position: "absolute", left: "37%", top: "50%"}
     const clickStyle = {visibility: "hidden"}
+
+    const content = (
+        <p style={{ padding: 0, margin: 0 }}>
+          Here is some content
+          <br />
+          <br />
+          More content
+          <br />
+          <br />
+          More content
+          <br />
+          <br />
+          More content
+          <br />
+          <br />
+          More content
+          <br />
+          <br />
+          More content
+          <br />
+          <br />
+          More content
+          <br />
+          <br />
+          This how we began
+        </p>
+      );
+      const heading = "Ordering Menu";
 
     return (
         <div>
-            <div 
-                style={ hovered1 && !isClicked ? hoverStyle : !hovered1 && !isClicked ? baseStyle : clickStyle }
-                onMouseEnter={() => setHovered1(true)}
-                onMouseLeave={() => setHovered1(false)}
-                onClick={() => navigate('/ShoppingCart', false)}
+            {textPopup ? (
+                <TextPopup content={content} cross={hideTextPopup} heading={heading} />
+            ) : null}
+            <button 
+                style={ hovered ? hoverStyle : baseStyle }
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+                onClick={showTextPopup}
             >
-                &#128308;
-            </div>
-            <div 
-                style={ hovered2 && !isClicked ? hoverStyle : !hovered2 && !isClicked ? baseStyle : clickStyle }
-                onMouseEnter={() => setHovered2(true)}
-                onMouseLeave={() => setHovered2(false)}
-                onClick={() => navigate('/ShoppingCart', false)}
-            >
-                &#128308;
-            </div>
-            <div 
-                style={ hovered3 && !isClicked ? hoverStyle : !hovered3 && !isClicked ? baseStyle : clickStyle }
-                onMouseEnter={() => setHovered3(true)}
-                onMouseLeave={() => setHovered3(false)}
-                onClick={() => navigate('/ShoppingCart', false)}
-            >
-                &#128308;
-            </div>
-            <div 
-                style={ hovered4 && !isClicked ? hoverStyle : !hovered4 && !isClicked ? baseStyle : clickStyle }
-                onMouseEnter={() => setHovered4(true)}
-                onMouseLeave={() => setHovered4(false)}
-                onClick={() => navigate('/ShoppingCart', false)}
-            >
-                &#128308;
-            </div>
-            { isClicked ? cart : <img 
+                ORDER
+            </button>
+            <img 
                 height='100%' 
                 width='100%' 
                 src={Ecuador} 
                 alt='' 
             />
-            }
         </div>
     )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,20 +2,24 @@ import React, { useState, useEffect } from 'react'
 import { navigate } from 'hookrouter'
 import Header from '../components/ui/Header'
 import Globe from '../components/ui/Globe'
+import Zoom from '../components/ui/Zoom'
 import Search from '../components/ui/Search'
 
 const Home = () => {
 
-    // const [videoEnded, setVideoEnded] = useState(false)
-    // const [location, setLocation] = useState('')
+    const [videoEnded, setVideoEnded] = useState(false)
+    const [location, setLocation] = useState('')
+
+    const zoom = <Zoom videoEnd={(q) => setVideoEnded(q)} />
+    const globe = <Globe />
 
     
     return (
-        <div className="Container" onClick={() => navigate('/GifPage', false)}>
+        <div className="Container">
             <Header />
-            <Globe />
+            { videoEnded && location.length > 0 ? navigate('/GifPage', false) : location.length > 0 ? zoom : globe }
             <br></br>
-            <Search />
+            { location.length == 0 ? <Search getLocation={(q) => setLocation(q)} /> : ''}
         </div>
         
     )
